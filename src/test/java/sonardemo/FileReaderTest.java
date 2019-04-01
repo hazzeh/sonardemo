cdd2fc841d9b5330c134fdbb1699e366ea1e3af7
Hazze <hans.hazelius@gmail.com>
2019-04-01 20:49:41 +0200

    Add more tests

--- src/test/java/sonardemo/FileReaderTest.java
+++ src/test/java/sonardemo/FileReaderTest.java
@@ -2,11 +2,15 @@ package sonardemo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class FileReaderTest {
 
     @Test
-    public void readInputfile() {
+    public void readInputfile() throws Exception {
+        FileReader sut = new FileReader();
+        assertThat(sut.readInputfile()).isEqualTo("This is an input file!;Line 2");
+
     }
 }
